5a509f310fa4e01663e18a86be50ce1cc206b502
Colin Grambow <cgrambow@mit.edu>
2018-04-09 17:21:23 -0400

    Add script for parsing structures in 134k dataset
    Calculate heats of formation and save list of structures to disk
    (without fluorine containing molecules).

---
+++ scripts/parse_134k.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import argparse
+import cPickle as pickle
+import glob
+import os
+
+from data.structure import Structure
+from data.energy_data import freq_scale_factors
+
+
+def main():
+    data_dir, out_file = parse_args()
+    structs = []
+
+    print('Parsing files...')
+    files = glob.iglob(os.path.join(data_dir, '*.xyz'))
+    for path in files:
+        s = Structure(path=path)
+        if 'F' not in s.elements:  # Don't use fluorine containing molecules
+            s.get_enthalpy_of_formation(freq_scale_factor=freq_scale_factors[s.model_chemistry],
+                                        apply_bond_corrections=False)
+            structs.append(s)
+
+    with open(out_file, 'wb') as f:
+        pickle.dump(structs, f, protocol=pickle.HIGHEST_PROTOCOL)
+        print('Dumped {} structures to {}'.format(len(structs), out_file))
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Parse 134k dataset.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('data_dir', metavar='DIR', help='Path to 134k data directory')
+    parser.add_argument('out_file', metavar='FILE', help='Path to output file')
+    args = parser.parse_args()
+
+    data_dir = args.data_dir
+    out_file = args.out_file
+
+    return data_dir, out_file
+
+
+if __name__ == '__main__':
+    main()
